e73f0ff3a02ca96c3c2c4930863656caee1ecd42
wait4ndbleed <73363131+wait4ndbleed@users.noreply.github.com>
2023-10-01 14:19:40 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className="overflow-hidden">{children}</body>
+      <body className="sm:overflow-hidden text-white">{children}</body>
     </html>
   )
 }
